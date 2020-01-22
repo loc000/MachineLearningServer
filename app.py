a626ac211b6fc68ad5140a786d618a2559814ef4
loc000 <160322449@stu.vtc.edu.hk>
2020-01-22 16:14:36 +0800

    .

--- app.py
+++ app.py
@@ -1,20 +1,13 @@
 import os
-
+import time
 import cv2
 import flask
 import numpy as np
-import tensorflow as tf
-from PIL import Image
+# from PIL import Image
 from flask import jsonify
 from flask import request
 
 cwd = os.getcwd()
-
-os.chdir("./ShowAttendAndTellModel/")
-print(os.getcwd())
-from ShowAttendAndTellModel.run_inference import CaptionInference
-
-os.chdir(cwd)
 
 os.chdir("./ssd_mobilenet_v2_oid_v4_2018_12_12/")
 cvNet = cv2.dnn.readNetFromTensorflow('frozen_inference_graph.pb', 'graph.pbtxt')
@@ -28,7 +21,7 @@ video_capture_dict = {}
 
 @app.route('/objectdetection', methods=['GET'])
 def fuck():
-    return "fuck"
+    return "Please use POST method for object detection"
 
 
 @app.route('/objectdetection', methods=['POST'])
@@ -38,8 +31,10 @@ def predict():
     height, width, _ = img.shape
     rows = img.shape[0]
     cols = img.shape[1]
+    t1 = time.time()
     cvNet.setInput(cv2.dnn.blobFromImage(img, size=(300, 300), swapRB=True, crop=False))
     cvOut = cvNet.forward()
+    print(time.time()-t1)
     output_result_list = []
     for detection in cvOut[0, 0, :, :]:
         score = float(detection[2])
@@ -78,34 +73,9 @@ def predict():
     # data["result"] = output_result_list
     # cv2.imshow("loc",img)
     # cv2.waitKey(1)
+    print(output_result_list)
     return jsonify(output_result_list)
 
-
-@app.route('/imagecaption', methods=['POST'])
-def imagecaption():
-    request_json = request.json
-    img = cv2.imdecode(np.frombuffer(request.data, np.uint8), cv2.IMREAD_UNCHANGED)
-    height, width, _ = img.shape
-    img_np = np.array(cap_infer.resize_image(Image.fromarray(cv2.cvtColor(img, cv2.COLOR_BGR2RGB)),
-                                             cap_infer.model.cnn.image_size)).astype(np.float32)
-    if cap_infer.use_inception:
-        img_np /= 255.0
-        img_np -= 0.5
-        img_np *= 2.0
-    if img_np.ndim == 2:
-        img_np = np.stack((img_np,) * 3, axis=-1)
-    alphas, betas, captions = cap_infer.inference_np(np.array([img_np]))
-    output_result_list = []
-    for alpha, beta, caption in zip(alphas, betas, captions):
-        # out_file.write("{}\t{}\n".format(fname, caption))
-        # if args.visualize:
-        #     visualize(alpha, beta, caption, fname, args.use_inception)
-        output_result_list.append({"caption": caption})
-    # data["result"] = output_result_list
-    return jsonify(output_result_list)
-
-
-if __name__ == "__main__":
-    sess = tf.Session()
-    cap_infer = CaptionInference(sess, "ShowAttendAndTellModel/model_best/model-best", use_inception=True)
-    app.run(host='0.0.0.0')
+#
+# if __name__ == "__main__":
+#     app.run(host='0.0.0.0',threaded=True)
