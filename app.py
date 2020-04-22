0e5f84821ab403a2e92b9b9c2414676b21ae4343
loc000 <liuonchun@live.hk>
2020-04-22 15:45:53 +0800

    fix output image logic

--- app.py
+++ app.py
@@ -22,6 +22,7 @@ from waitress import serve
 
 cwd = os.getcwd()
 import tensorflow.compat.v1 as tf
+
 tf.disable_v2_behavior()
 if enable_image_caption:
     # import tensorflow.compat.v1 as tf
@@ -29,14 +30,15 @@ if enable_image_caption:
     os.chdir("./ShowAttendAndTellModel/")
     print(os.getcwd())
     from ShowAttendAndTellModel.run_inference import CaptionInference
-    os.chdir(cwd)
 
+    os.chdir(cwd)
 
 os.chdir("./ssd_mobilenet_v2_oid_v4_2018_12_12/")
 model_weight_file = 'frozen_inference_graph.pb'
 model_graph_file = 'graph.pbtxt'
 if not os.path.isfile(model_weight_file):
     import wget
+
     wget.download(
         "https://github.com/loc000/MachineLearningServer/releases/download/ssd_mobilenet_v2_oid_v4_2018_12_12/frozen_inference_graph.pb",
         out=model_weight_file)
@@ -61,6 +63,7 @@ my_ip = s.getsockname()[0]
 frame_no = 0
 filter_class = list(csv.reader(open(filter_class_label_file, encoding="utf-8"), delimiter=','))
 
+
 @app.route('/objectdetection', methods=['POST'])
 def predict():
     global frame_no
@@ -69,38 +72,36 @@ def predict():
     frame_no = frame_no + 1
     request_json = request.json
     img = cv2.imdecode(np.frombuffer(request.data, np.uint8), cv2.IMREAD_UNCHANGED)
-    print(img.shape)
     height, width, _ = img.shape
-    depth= np.asarray(cv2.resize(test2.predict(img),(width,height)))
+    depth = np.asarray(cv2.resize(test2.predict(img), (width, height)))
     rows = img.shape[0]
     cols = img.shape[1]
     cvNet.setInput(cv2.dnn.blobFromImage(img, size=(300, 300), swapRB=True, crop=False))
     cvOut = cvNet.forward()
     # Put efficiency information.
     t, _ = cvNet.getPerfProfile()
-    label = 'Inference time: %.2f ms' % (t * 1000.0 / cv2.getTickFrequency())
-    print(label)
+    print('Inference time: %.2f ms' % (t * 1000.0 / cv2.getTickFrequency()))
     output_result_list = []
     if save_debug_output:
         display_img = img.copy()
-    for detection in cvOut[0, 0, :, :]:
+    for key,detection in enumerate(cvOut[0, 0, :, :]):
         score = float(detection[2])
         className = classList[int(detection[1]) - 1]
         global label_list
         label_list = filter_class[int(detection[1])]
         img_write = img.copy()
         if int(label_list[2]) > 0 and score > 0.3:
-            if len(label_list)>3 and score< float(label_list[3]):
-                break
+            if len(label_list) > 3 and score < float(label_list[3]):
+                continue
             left = max(detection[3] * cols, 0)
             top = max(detection[4] * rows, 0)
             right = detection[5] * cols
             bottom = detection[6] * rows
-            
-            if save_debug_output:
 
+            if save_debug_output:
                 cv2.rectangle(img_write, (int(left), int(top)), (int(right), int(bottom)), (23, 230, 210), thickness=2)
-                cv2.rectangle(display_img, (int(left), int(top)), (int(right), int(bottom)), (23, 230, 210), thickness=2)
+                cv2.rectangle(display_img, (int(left), int(top)), (int(right), int(bottom)), (23, 230, 210),
+                              thickness=2)
 
                 # print(classList[int(detection[1]) - 1])
 
@@ -111,8 +112,8 @@ def predict():
 
             mid_x, mid_y = left + (right - left) / 2, top + (bottom - top) / 2
             loc = ""
-            depth2=np.average(depth[int(top):int(bottom),int(left):int(right)])
-            priority=int(label_list[2])*int(depth2)
+            depth2 = np.average(depth[int(top):int(bottom), int(left):int(right)])
+            priority = int(label_list[2]) * int(depth2)
             if mid_y < height / 3:
                 loc += "Top "
             elif mid_y > height / 3 * 2:
@@ -132,23 +133,32 @@ def predict():
                                        "right": int(right),
                                        "bottom": int(bottom),
                                        "location_description": loc,
-                                       "depth": depth2
+                                       "depth": depth2,
+                                       "priority": priority
                                        })
             if save_debug_output:
                 output_object_folder = "output/byObject/" + className + label_list[1] + "/"
                 if not os.path.isdir(output_object_folder):
                     os.makedirs(output_object_folder, exist_ok=True)
-                output_frame_folder = "output/byFrame/"
-                if not os.path.isdir(output_frame_folder):
-                    os.makedirs(output_frame_folder, exist_ok=True)
-                cv2.imwrite(output_object_folder + "frameNo_" + str(frame_no).zfill(6) + "_score_" + str(score) + ".jpg", img_write)
-                cv2.imwrite(output_frame_folder + "frameNo_" + str(frame_no).zfill(6)+ ".jpg", display_img)
+                # output_frame_folder = "output/byFrame/"
+                # if not os.path.isdir(output_frame_folder):
+                #     os.makedirs(output_frame_folder, exist_ok=True)
+                cv2.imwrite(
+                    output_object_folder + "frameNo_" + str(frame_no).zfill(6) +"_key_"+str(key)+ ".jpg",
+                    img_write)
+                # cv2.imwrite(output_frame_folder + "frameNo_" + str(frame_no).zfill(6) + ".jpg", display_img)
     # data["result"] = output_result_list
     if save_debug_output:
+        output_frame_folder = "output/byFrame/"
+        if not os.path.isdir(output_frame_folder):
+            os.makedirs(output_frame_folder, exist_ok=True)
+        cv2.imwrite(output_frame_folder + "frameNo_" + str(frame_no).zfill(6) + ".jpg", display_img)
         cv2.imshow("result", display_img)
         cv2.waitKey(1)
-    # print(output_result_list)
+        pass
+    print(output_result_list)
     return jsonify(output_result_list)
+
 
 if enable_image_caption:
     @app.route('/imagecaption', methods=['POST'])
@@ -177,7 +187,6 @@ if enable_image_caption:
         # print(output_result_list)
         return jsonify({"caption": out_caption})
 
- 
 if __name__ == "__main__":
     sess = tf.Session()
     keras.backend.set_session(sess)
@@ -192,10 +201,10 @@ if __name__ == "__main__":
                                          "image_caption_machine_learning_server.tcp.local.")
 
     object_detection_info = ServiceInfo("_oml._tcp.local.",
-                                    "_oml._tcp.local.",
-                                    socket.inet_aton(my_ip), port, 0, 0,
-                                    {'type': 'objectdetection'},
-                                    "object_detection_machine_learning_server.tcp.local.")
+                                        "_oml._tcp.local.",
+                                        socket.inet_aton(my_ip), port, 0, 0,
+                                        {'type': 'objectdetection'},
+                                        "object_detection_machine_learning_server.tcp.local.")
 
     zeroconf = Zeroconf()
     zeroconf.register_service(object_detection_info)
